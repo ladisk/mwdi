49b4806c03a6f69b88609877f517186cd04e4c14
Janko Slavič <janko.slavic@gmail.com>
2022-03-14 09:31:59 +0100

    polishing

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -27,9 +27,13 @@ def test_sythetic(fs=1000, n=1000, fr=100, damping_ratio=0.01, phase=0.3, amplit
     k = 10
     identifier = mwdi.MorletWave(free_response=signal, fs=fs, k=k, n_1=n_1, n_2=n_2)
     w = 2*np.pi*fr
+    
     w_ident = identifier.find_natural_frequency(w=w, k=k, n=n_1)
+    np.testing.assert_equal(w_ident, w)
     print(f'w={w}, w_ident={w_ident}')
+
     damping_ratio_ident = identifier.identify_damping(w=w)
+    np.testing.assert_equal(damping_ratio_ident, damping_ratio) 
     print(f'damping_ratio={damping_ratio}, damping_ratio_ident:{damping_ratio_ident}')
 
 
